a4462205f565a88e1af3d8ba8403d17fd3c4edc6
KALENS-LAPTOP\knplu <knplummer1@gmail.com>
2021-11-27 12:14:01 -0500

    Add css framework and js file

--- SpotifyPlayer/src/index.js
+++ SpotifyPlayer/src/index.js
@@ -9,8 +9,11 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html of the app.
